8ebe893453881236e6c51e265a9a4261654442b7
Brian Woodward <brian.woodward@gmail.com>
2015-03-04 13:43:38 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * dronetest <https://github.com/doowb/dronetest>
+ *
+ * Copyright (c) 2015 Brian Woodward.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
